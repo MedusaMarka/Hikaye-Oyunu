7e61dbc02876b142c80be1e6ac1b00535b07e743
MedusaMarka <156526848+MedusaMarka@users.noreply.github.com>
2024-01-13 18:52:28 +0300

    Update guncelle.py

--- guncelle.py
+++ guncelle.py
@@ -1,6 +1,13 @@
-import git 
+import os
+import shutil
 
-# Instantiate repo object
-repo = git.Repo("https://github.com/MedusaMarka/Hikaye-Oyunu.git")
-# pull down 
-repo.remotes.origin.pull()
+# Repo bilgileri
+repo_url = 'https://github.com/MedusaMarka/Hikaye-Oyunu.git'  # Kullanıcı adı ve repo adı ile değiştirin
+klonlanacak_dizin = os.getcwd()  # os.getcwd() mevcut çalışma dizinin tam yolunu verir
+
+# Eğer dizin zaten varsa, sil
+if os.path.exists(klonlanacak_dizin):
+    shutil.rmtree(klonlanacak_dizin)
+
+# Repo'yu klonla
+os.system(f'git clone {repo_url} {klonlanacak_dizin}')
