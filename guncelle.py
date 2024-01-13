2ef6742a614a71c5c57ca32103fdba313397f147
MedusaMarka <156526848+MedusaMarka@users.noreply.github.com>
2024-01-13 19:47:10 +0300

    Update guncelle.py

--- guncelle.py
+++ guncelle.py
@@ -2,5 +2,6 @@ import os
 import shutil 
 os.system(f'chmod +x uninstall')
 os.system(f'./uninstall')
+os.system(f'cd')
 os.system(f'git clone https://github.com/MedusaMarka/Hikaye-Oyunu.git')
 os.system(f'cd Hikaye-Oyunu')
